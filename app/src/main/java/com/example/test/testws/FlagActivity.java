55105da2217db867419ee7a08b5c4c8ed67a42b0
d.yacenko <d.yacenko@gmail.com>
2019-08-23 12:37:21 +0300

    Test workspace with android project depicting the flag of Indonesia

---
+++ app/src/main/java/com/example/test/testws/FlagActivity.java
@@ -0,0 +1,13 @@
+package com.example.test.testws;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FlagActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.flag);
+    }
+}
